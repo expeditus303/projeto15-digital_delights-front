cc501386e0ef61acc1e273d23240f217c114023e
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-01-28 09:32:36 -0300

    change ListProducts state from home to app and create apiProducts

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,17 +1,9 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import Loading from "../../components/Loading"
 import ProductCard from "../../components/ProductCard"
 import { ProductsConteiner, TopMenu } from "./HomePageCss"
 
-export default function HomePage() {
-    const [listProducts, setListProducts] = useState(undefined)
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_API_URL}/`)
-            .then((res) => setListProducts(res.data))
-            .catch((err) => console.log(err))
-    }, [])
+export default function HomePage({listProducts}) {
 
     if (listProducts === undefined) return <Loading></Loading>
     return (
