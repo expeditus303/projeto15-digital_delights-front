4cf507273c734640db7a5dad5b7b4505b7906a78
expeditus303 <ricardotonietto@gmail.com>
2023-01-30 16:44:11 -0300

    pull

--- src/pages/CheckOutPage/CheckOutCss.js
+++ src/pages/CheckOutPage/CheckOutCss.js
@@ -1,127 +1,139 @@
 import styled from "styled-components";
 
 export const CheckoutContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-background-color: #FFF;
-/* height: 100vh; */
-padding: 50px 0 46px;
-min-height: 100vh;
-width: 100vw;
-max-width: 412px;
-position: relative;
-`
+  display: ${(props) => (props.showSuccess ? "none" : "flex")};
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  /* height: 100vh; */
+  padding: 50px 0 46px;
+  min-height: 100vh;
+  width: 100vw;
+  max-width: 412px;
+  position: relative;
+`;
 export const Header = styled.div`
-position:fixed;
-top:0;
-display: flex;
-width: 100vw;
-max-width: 412px;
-height: 50px;
-flex-direction: row;
-justify-content: space-between;
-padding: 0 20px;
-align-items: center;
-background-color: #000;
+  position: fixed;
+  top: 0;
+  display: flex;
+  width: 100vw;
+  max-width: 412px;
+  height: 50px;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 20px;
+  align-items: center;
+  background-color: #000;
 
-ion-icon{
+  ion-icon {
     font-size: 25px;
     color: #fff;
-}
+  }
 
-a{
+  a {
     color: #fff;
-}
-`
+  }
+`;
 export const Title = styled.h1`
-font-family: Tenor Sans;
-font-style: normal;
-font-weight: 400;
-font-size: 18px;
-color: #fff;
-`
+  font-family: Tenor Sans;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  color: #fff;
+`;
 export const Subtitle = styled.h2`
-font-family: Tenor Sans;
-font-style: normal;
-font-weight: 400;
-font-size: 35px;
-color: #000;
-margin-top: 50px;
-`
+  font-family: Tenor Sans;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 35px;
+  color: #000;
+  margin-top: 50px;
+`;
 export const Text = styled.div`
-display: flex;
-flex-direction: column;
-width: 315px;
-margin: 30px auto;
-justify-content: center;
+  display: flex;
+  flex-direction: column;
+  width: 315px;
+  margin: 30px auto;
+  justify-content: center;
 
-h3{
+  h3 {
     font-family: Tenor Sans;
     font-style: normal;
     font-weight: 400;
     font-size: 15px;
     color: #000;
-}
-`
+  }
+`;
 
 export const Line = styled.div`
-margin: 0 auto;
-display: flex;
-background-color: #000;
-width: 250px;
-height: 0.7px;
-margin-bottom: 20px;
-margin-top: 20px;
-`
+  margin: 0 auto;
+  display: flex;
+  background-color: #000;
+  width: 250px;
+  height: 0.7px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+`;
 
 export const CheckOutButton = styled.button`
-width: 100vw;
-max-width: 412px;
-margin: auto;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 46px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    pointer-events: ${(props) => (props.disabled ? "none" : "all")};
-    opacity: ${(props) => (props.disabled ? 0.7 : 1)};
-    font-size: 20px;
-    line-height: 26px;
-    text-align: center;
-    background: #000;
-    &:hover {
-        background: #E8EBEF;
-    }
-    s &:active {
-        background: #E8EBEF;
-    }
-    &:focus {
-        outline: none;
-    }
-    font-family: Tenor Sans;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 18px;
-    line-height: 23px;
-    color: #fff;
-
-`
+  width: 100vw;
+  max-width: 412px;
+  margin: auto;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 46px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  pointer-events: ${(props) => (props.disabled ? "none" : "all")};
+  opacity: ${(props) => (props.disabled ? 0.7 : 1)};
+  font-size: 20px;
+  line-height: 26px;
+  text-align: center;
+  background: #000;
+  &:hover {
+    background: #e8ebef;
+  }
+  s &:active {
+    background: #e8ebef;
+  }
+  &:focus {
+    outline: none;
+  }
+  font-family: Tenor Sans;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 23px;
+  color: #fff;
+`;
 
 export const CartOverall = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-around;
-align-items: center;
-width: 300px;
-flex-wrap: wrap;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 300px;
+  flex-wrap: wrap;
+`;
 export const Product = styled.div`
-display: flex;
-width: 100px;
-height: 115px;
-`
+  display: flex;
+  width: 100px;
+  height: 115px;
+`;
+
+export const SuccessContainer = styled.div`
+  display: ${(props) => (props.showSuccess ? "flex" : "none")};
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  /* height: 100vh; */
+  padding: 50px 0 46px;
+  min-height: 100vh;
+  width: 100vw;
+  max-width: 412px;
+  background-color: yellow;
+`;
--- src/pages/CheckOutPage/CheckOutPage.js
+++ src/pages/CheckOutPage/CheckOutPage.js
@@ -1,82 +1,103 @@
-import { Header, CheckoutContainer, Title, Subtitle, Text, Line, CheckOutButton, CartOverall, Product } from "./CheckOutCss";
+import {
+  Header,
+  CheckoutContainer,
+  Title,
+  Subtitle,
+  Text,
+  Line,
+  CheckOutButton,
+  CartOverall,
+  Product,
+  SuccessContainer,
+} from "./CheckOutCss";
 import { Link, useNavigate } from "react-router-dom";
 import apiCheckout from "../../services/apiCheckOut";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../context/UserContext.js";
 import apiCart from "../../services/apiCart";
 import CartList from "../../components/CarList";
-
+import { BsCheck2Circle } from 'react-icons/bs';
 
 export default function CheckOutPage({ paymentInfo, listProducts }) {
-    const { user } = useContext(UserContext);
-    const navigate = useNavigate()
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
 
-    async function submitOrder() {
-        console.log(user)
+  const [showSuccess, setShowSuccess] = useState(false);
 
-        try {
-            const checkPayment = await apiCheckout.sendPaymentMethod(paymentInfo, user)
-            console.log(checkPayment.data);
-            console.log(user)
-            
-        } catch (error) {
-            console.log(user)
-            return console.log(error)
-        }
+  async function submitOrder() {
+    console.log(user);
+    setShowSuccess(true);
 
-        try {
-            const checkEmpty = await apiCart.emptyCart(user)
-            console.log(checkEmpty.data)
-        } catch (error) {
-            console.log(error)
-        }
+    try {
+      const checkPayment = await apiCheckout.sendPaymentMethod(
+        paymentInfo,
+        user
+      );
+      console.log(checkPayment.data);
+      console.log(user);
+    } catch (error) {
+      console.log(user);
+      return console.log(error);
     }
 
-    return (
-        <CheckoutContainer>
+    try {
+      const checkEmpty = await apiCart.emptyCart(user);
+      console.log(checkEmpty.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-            <Header>
-                <Link to="/">
-                    <Title>
-                        Digital
-                        <br />Delights
-                    </Title>
-                </Link>
+  return (
+    <>
+        <SuccessContainer showSuccess={showSuccess}>
+        <div>
+            <h1>Payment success</h1>
+            <BsCheck2Circle />
+            <h2>Your payment was success</h2>
+            <p>Payment ID {}</p>
+        </div>
+        </SuccessContainer>
+      <CheckoutContainer showSuccess={showSuccess}>
+        <Header>
+          <Link to="/">
+            <Title>
+              Digital
+              <br />
+              Delights
+            </Title>
+          </Link>
 
-                <Link to="/cart">
-                    <ion-icon name="cart-outline"></ion-icon>
-                </Link>
-            </Header>
+          <Link to="/cart">
+            <ion-icon name="cart-outline"></ion-icon>
+          </Link>
+        </Header>
 
-            <Subtitle>
-                CHECKOUT
-            </Subtitle>
+        <Subtitle>CHECKOUT</Subtitle>
 
-            <Text>
-                <h3>
-                    Hello, Rafaela Oliveira!
-                    <br />
-                    <br />
-                    Please, confirm all the information below before submitting your purchase:
-                    <br />
-                    <br />
-                    Payment Method:
-                    <br />
-                    Credit Card ending in 4242
-                </h3>
+        <Text>
+          <h3>
+            Hello, Rafaela Oliveira!
+            <br />
+            <br />
+            Please, confirm all the information below before submitting your
+            purchase:
+            <br />
+            <br />
+            Payment Method:
+            <br />
+            Credit Card ending in 4242
+          </h3>
 
-                <Line></Line>
+          <Line></Line>
 
-                <h3>Your cart overall:</h3>
-            </Text>
+          <h3>Your cart overall:</h3>
+        </Text>
 
-            <CartList listProducts={listProducts}></CartList>
+        <CartList listProducts={listProducts}></CartList>
 
-            <CheckOutButton onClick={() => submitOrder()}>
-                CHECKOUT
-            </CheckOutButton>
-
-        </CheckoutContainer>
-
-    )
+        <CheckOutButton onClick={() => submitOrder()}>CHECKOUT</CheckOutButton>
+      </CheckoutContainer>
+    </>
+  );
 }
